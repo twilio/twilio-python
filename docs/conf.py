b8ef8e1ca01196553cc47c4d367e3f73f9cf93b2
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-07-15 15:52:00 -0700

    Change copyright

--- docs/conf.py
+++ docs/conf.py
@@ -46,8 +46,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'twilio-python2'
-copyright = u'2011, Kyle Conroy'
+project = u'twilio-python'
+copyright = u'2011, Twilio Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -173,7 +173,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'twilio-python2doc'
+htmlhelp_basename = 'twilio-pythondoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -187,8 +187,8 @@ htmlhelp_basename = 'twilio-python2doc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'twilio-python2.tex', u'twilio-python2 Documentation',
-   u'Kyle Conroy', 'manual'),
+  ('index', 'twilio-python.tex', u'twilio-python Documentation',
+   u'Twilio Inc.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -220,18 +220,18 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'twilio-python2', u'twilio-python2 Documentation',
-     [u'Kyle Conroy'], 1)
+    ('index', 'twilio-python', u'twilio-python Documentation',
+     [u'Twilio Inc.'], 1)
 ]
 
 
 # -- Options for Epub output ---------------------------------------------------
 
 # Bibliographic Dublin Core info.
-epub_title = u'twilio-python2'
-epub_author = u'Kyle Conroy'
-epub_publisher = u'Kyle Conroy'
-epub_copyright = u'2010, Kyle Conroy'
+epub_title = u'twilio-python'
+epub_author = u'kyle@twilio.com'
+epub_publisher = u'Twilio Inc.'
+epub_copyright = u'2010, Twilio Inc.'
 
 # The language of the text. It defaults to the language option
 # or en if the language is not set.
