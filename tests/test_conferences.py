b6b9247f32240999c8e71b6d183e229b656c4219
Kyle Conroy <kyle.j.conroy@gmail.com>
2011-09-21 02:42:40 -0700

    Fix dates when fitlering resources. Add a changelog

---
+++ tests/test_conferences.py
@@ -0,0 +1,31 @@
+import unittest
+from datetime import date
+from mock import patch, Mock
+from twilio.rest.resources import Conferences
+
+
+class ConferenceTest(unittest.TestCase):
+
+    def setUp(self):
+        self.resource = Conferences("foo", ("sid", "token"))
+
+    def test_list_after(self):
+        self.resource.get_instances = Mock()
+        self.resource.list(created_after=date(2011, 1, 1))
+        self.resource.get_instances.assert_called_with(params={
+            "DateCreated>": "2011-01-01"})
+
+    def test_list_on(self):
+        self.resource.get_instances = Mock()
+        self.resource.list(created=date(2011, 1, 1))
+        self.resource.get_instances.assert_called_with(params={
+            "DateCreated=": "2011-01-01"})
+
+    def test_list_before(self):
+        self.resource.get_instances = Mock()
+        self.resource.list(created_before=date(2011, 1, 1))
+        self.resource.get_instances.assert_called_with(params={
+            "DateCreated<": "2011-01-01"})
+
+
+
