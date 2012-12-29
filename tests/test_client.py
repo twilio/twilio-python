c159fec95fddce3069ad8ec2631c7b205cef9f7a
Kevin Burke <kevin@twilio.com>
2012-12-28 16:46:11 -0800

    add client.members() function to get queue members.

--- tests/test_client.py
+++ tests/test_client.py
@@ -6,7 +6,10 @@ else:
 
 from twilio.rest import TwilioRestClient, resources
 from mock import patch, Mock
+from tools import create_mock_json
 
+
+AUTH = ("ACCOUNT_SID", "AUTH_TOKEN")
 
 class RestClientTest(unittest.TestCase):
 
@@ -18,7 +21,7 @@ class RestClientTest(unittest.TestCase):
         self.client.request("2010-04-01", method="GET")
         mock.assert_called_with("GET", "https://api.twilio.com/2010-04-01",
             headers={"User-Agent": 'twilio-python'}, params={},
-            auth=("ACCOUNT_SID", "AUTH_TOKEN"), data=None)
+            auth=AUTH, data=None)
 
     def test_connect_apps(self):
         self.assertIsInstance(self.client.connect_apps,
@@ -34,3 +37,11 @@ class RestClientTest(unittest.TestCase):
         mock.return_value.ok = True
         mock.return_value.content = '{"conferences": []}'
         self.client.conferences.list()
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_members(self, mock):
+        resp = create_mock_json("tests/resources/members_list.json")
+        mock.return_value = resp
+        self.client.members("QU123").list()
+        uri = "https://api.twilio.com/2010-04-01/Accounts/ACCOUNT_SID/Queues/QU123/Members"
+        mock.assert_called_with("GET", uri, params={}, auth=AUTH)
