7868a50abe780a5fbc6f85961febd59686770cb7
Kevin Burke <kevin@twilio.com>
2013-05-21 09:42:33 -0700

    Bump version to 3.5.0

--- twilio/__init__.py
+++ twilio/__init__.py
@@ -1,4 +1,4 @@
-__version_info__ = ('3', '4', '5')
+__version_info__ = ('3', '5', '0')
 __version__ = '.'.join(__version_info__)
 
 
@@ -16,3 +16,4 @@ class TwilioRestException(TwilioException):
 
     def __str__(self):
         return "HTTP ERROR %s: %s \n %s" % (self.status, self.msg, self.uri)
+
