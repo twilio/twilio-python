70b559d29e40369fac4131234f090ccbda53d2f5
Gustaf Helgesson <hontonilinux@gmail.com>
2013-08-20 23:05:24 -0700

    Connection update.

--- twilio/rest/resources/connection.py
+++ twilio/rest/resources/connection.py
@@ -1,5 +1,6 @@
 from twilio.rest.resources.imports import httplib2
 
+
 class Connection(object):
     _proxy_info = None
 
@@ -9,4 +10,5 @@ class Connection(object):
 
     @classmethod
     def set_proxy_info(cls, proxy_url, proxy_port):
-        cls._proxy_info = httplib2.ProxyInfo(httplib2.socks.PROXY_TYPE_HTTP, proxy_url, proxy_port)
+        cls._proxy_info = httplib2.ProxyInfo(httplib2.socks.PROXY_TYPE_HTTP,
+                                             proxy_url, proxy_port)
