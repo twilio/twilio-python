3f722e83fba85ef96f10d82f9f2e436017d7b544
Doug Black <dblack@twilio.com>
2013-09-25 12:05:53 -0700

    use default background color

--- twilio/rest/resources/base.py
+++ twilio/rest/resources/base.py
@@ -132,19 +132,19 @@ def make_twilio_request(method, uri, **kwargs):
             message = resp.content
 
         def red(msg):
-            return u"\033[31m\033[40m%s\033[0m" % msg
+            return u"\033[31m\033[49m%s\033[0m" % msg
 
         def white(msg):
-            return u"\033[37m\033[40m%s\033[0m" % msg
+            return u"\033[37m\033[49m%s\033[0m" % msg
 
         def blue(msg):
-            return u"\033[34m\033[40m%s\033[0m" % msg
+            return u"\033[34m\033[49m%s\033[0m" % msg
 
         def orange(msg):
-            return u"\033[33m\033[40m%s\033[0m" % msg
+            return u"\033[33m\033[49m%s\033[0m" % msg
 
         def teal(msg):
-            return u"\033[36m\033[40m%s\033[0m" % msg
+            return u"\033[36m\033[49m%s\033[0m" % msg
 
         # If it makes sense to print a human readable error message, try to do
         # it. The one problem is that someone might catch this error and try to
