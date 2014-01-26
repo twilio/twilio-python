d7f6d223df3ae7a2007b7123ba2bfc5799510c09
Kevin Burke <kevin@twilio.com>
2014-01-26 12:33:09 -0800

    spacing in new test file

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -4,9 +4,11 @@ from mock import patch
 from twilio import TwilioException
 from twilio.rest import TwilioRestClient, find_credentials
 
+
 def test_creds_not_found():
     """ I shouldn't find credentials if they are not there """
     assert_equal(find_credentials({'foo': 'bar'}), (None, None))
+
 
 def test_find_creds():
     """ I should find the credentials if they are there """
@@ -16,6 +18,7 @@ def test_find_creds():
         'TWILIO_AUTH_TOKEN': '456',
     }
     assert_equal(find_credentials(env), ('AC123', '456'))
+
 
 @patch("twilio.rest.find_credentials")
 def test_creds_error(creds):
