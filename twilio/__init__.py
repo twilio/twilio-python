21217183e5e3b1919ac20606e6d12c0c28e415db
Paul Hallett <hello@phalt.co>
2014-09-29 16:09:17 +0100

    Fix Python3 compatibility

--- twilio/__init__.py
+++ twilio/__init__.py
@@ -6,6 +6,6 @@ from six import u
 # Backwards compatibility.
 from .version import __version__, __version_info__
 
-from exceptions import TwilioException, TwimlException
+from .exceptions import TwilioException, TwimlException
 
-from rest.exceptions import TwilioRestException
+from .rest.exceptions import TwilioRestException
