332a90793d9c099fd12eb36d7206d06289bbb93f
Ragil Prasetya <rprasetya@twilio.com>
2015-07-17 10:07:55 -0700

    Fix broken tests
    1. Remove global state in queue test causing failures depending on execution order
    2. Fix messages test

--- tests/test_queues.py
+++ tests/test_queues.py
@@ -1,3 +1,4 @@
+import unittest
 from mock import patch
 from tests.tools import create_mock_json
 from twilio.rest.resources import Queues, Queue
@@ -8,67 +9,70 @@ AUTH = (ACCOUNT_SID, "token")
 QUEUE_SID = "QU1b9faddec3d54ec18488f86c83019bf0"
 CALL_SID = "CAaaf2e9ded94aba3e57c42a3d55be6ff2"
 
-list_resource = Queues(BASE_URI, AUTH)
-instance_resource = Queue(list_resource, QUEUE_SID)
+
+class QueueTest(unittest.TestCase):
+
+    def setUp(self):
+        self.list_resource = Queues(BASE_URI, AUTH)
+        self.instance_resource = Queue(self.list_resource, QUEUE_SID)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_queues_list(self, mock):
+        resp = create_mock_json("tests/resources/queues_list.json")
+        mock.return_value = resp
+
+        uri = "%s/Queues" % (BASE_URI)
+        self.list_resource.list()
+
+        mock.assert_called_with("GET", uri, params={}, auth=AUTH,
+                                use_json_extension=True)
 
 
-@patch("twilio.rest.resources.base.make_twilio_request")
-def test_queues_list(mock):
-    resp = create_mock_json("tests/resources/queues_list.json")
-    mock.return_value = resp
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_queues_create(self, mock):
+        resp = create_mock_json("tests/resources/queues_instance.json")
+        resp.status_code = 201
+        mock.return_value = resp
 
-    uri = "%s/Queues" % (BASE_URI)
-    list_resource.list()
+        uri = "%s/Queues" % (BASE_URI)
+        self.list_resource.create('test', max_size=9001)
 
-    mock.assert_called_with("GET", uri, params={}, auth=AUTH,
-                            use_json_extension=True)
-
-
-@patch("twilio.rest.resources.base.make_twilio_request")
-def test_queues_create(mock):
-    resp = create_mock_json("tests/resources/queues_instance.json")
-    resp.status_code = 201
-    mock.return_value = resp
-
-    uri = "%s/Queues" % (BASE_URI)
-    list_resource.create('test', max_size=9001)
-
-    mock.assert_called_with("POST", uri,
-                            data={'FriendlyName': 'test', 'MaxSize': 9001},
-                            auth=AUTH,
-                            use_json_extension=True)
+        mock.assert_called_with("POST", uri,
+                                data={'FriendlyName': 'test', 'MaxSize': 9001},
+                                auth=AUTH,
+                                use_json_extension=True)
 
 
-@patch("twilio.rest.resources.base.make_twilio_request")
-def test_queues_get(mock):
-    resp = create_mock_json("tests/resources/queues_instance.json")
-    mock.return_value = resp
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_queues_get(self, mock):
+        resp = create_mock_json("tests/resources/queues_instance.json")
+        mock.return_value = resp
 
-    uri = "%s/Queues/%s" % (BASE_URI, QUEUE_SID)
-    list_resource.get(QUEUE_SID)
-    mock.assert_called_with("GET", uri, auth=AUTH,
-                            use_json_extension=True)
-
-
-@patch("twilio.rest.resources.base.make_twilio_request")
-def test_queue_update(mock):
-    resp = create_mock_json("tests/resources/queues_instance.json")
-    mock.return_value = resp
-
-    uri = "%s/Queues/%s" % (BASE_URI, QUEUE_SID)
-    instance_resource.update(friendly_name='QQ')
-
-    mock.assert_called_with("POST", uri,
-                            data={'FriendlyName': 'QQ'}, auth=AUTH,
-                            use_json_extension=True)
+        uri = "%s/Queues/%s" % (BASE_URI, QUEUE_SID)
+        self.list_resource.get(QUEUE_SID)
+        mock.assert_called_with("GET", uri, auth=AUTH,
+                                use_json_extension=True)
 
 
-@patch("twilio.rest.resources.base.make_twilio_request")
-def test_queue_delete(mock):
-    resp = create_mock_json("tests/resources/queues_instance.json")
-    mock.return_value = resp
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_queue_update(self, mock):
+        resp = create_mock_json("tests/resources/queues_instance.json")
+        mock.return_value = resp
 
-    uri = "%s/Queues/%s" % (BASE_URI, QUEUE_SID)
-    instance_resource.delete()
+        uri = "%s/Queues/%s" % (BASE_URI, QUEUE_SID)
+        self.instance_resource.update(friendly_name='QQ')
 
-    mock.assert_called_with("DELETE", uri, auth=AUTH, use_json_extension=True)
+        mock.assert_called_with("POST", uri,
+                                data={'FriendlyName': 'QQ'}, auth=AUTH,
+                                use_json_extension=True)
+
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_queue_delete(self, mock):
+        resp = create_mock_json("tests/resources/queues_instance.json")
+        mock.return_value = resp
+
+        uri = "%s/Queues/%s" % (BASE_URI, QUEUE_SID)
+        self.instance_resource.delete()
+
+        mock.assert_called_with("DELETE", uri, auth=AUTH, use_json_extension=True)
