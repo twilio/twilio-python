d7f22ebcda61d444573956e95b03d10903cea260
Ragil Prasetya <rprasetya@twilio.com>
2015-07-20 15:54:32 -0700

    Refactor phone numbers test to assert http request

--- tests/test_phone_numbers.py
+++ tests/test_phone_numbers.py
@@ -2,7 +2,8 @@ from __future__ import with_statement
 import json
 import unittest
 
-from mock import Mock
+from mock import patch, Mock
+from tests.tools import create_mock_json
 
 from twilio.rest.resources import PhoneNumbers
 from twilio.rest.resources import PhoneNumber
@@ -12,88 +13,119 @@ class PhoneNumberTest(unittest.TestCase):
 
     def setUp(self):
         self.parent = Mock()
-        self.uri = "/base"
+        self.base_uri = 'http://api.twilio.com/Accounts/ac1'
         self.auth = ("AC123", "token")
+        self.sid = 'phone_sid'
 
-    def test_update_rename_status_callback_url(self):
-        mock = Mock()
-        mock.uri = "/base"
-        instance = PhoneNumber(mock, "SID")
-        instance.update(status_callback_url="http://www.example.com")
-        mock.update.assert_called_with("SID", status_callback="http://www.example.com")
+        self.list = PhoneNumbers(self.base_uri, self.auth)
+        self.instance = PhoneNumber(self.list, self.sid)
 
-    def test_update_instance_rename_status_callback_url(self):
-        resource = PhoneNumbers(self.uri, self.auth)
-        resource.update_instance = Mock()
-        resource.update("SID", status_callback_url="http://www.example.com")
-        resource.update_instance.assert_called_with("SID", {"status_callback": "http://www.example.com"})
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_update_rename_status_callback_url(self, mock):
+        resp = create_mock_json('tests/resources/'
+                                'incoming_phone_numbers_instance.json')
+        resp.status_code = 201
+        mock.return_value = resp
 
-    def test_application_sid(self):
-        resource = PhoneNumbers(self.uri, self.auth)
-        resource.update_instance = Mock()
-        resource.update("SID", application_sid="foo")
-        resource.update_instance.assert_called_with(
-                "SID", {"voice_application_sid": "foo", "sms_application_sid": "foo"})
+        self.instance.update(status_callback_url="http://www.example.com")
 
-    def test_voice_application_sid(self):
-        resource = PhoneNumbers(self.uri, self.auth)
-        resource.update_instance = Mock()
-        resource.update("SID", voice_application_sid="foo")
-        resource.update_instance.assert_called_with(
-                "SID", {"voice_application_sid": "foo"})
+        uri = '%s/IncomingPhoneNumbers/%s' % (self.base_uri, self.sid)
 
-    def test_sms_application_sid(self):
-        resource = PhoneNumbers(self.uri, self.auth)
-        resource.update_instance = Mock()
-        resource.update("SID", sms_application_sid="foo")
-        resource.update_instance.assert_called_with(
-                "SID", {"sms_application_sid": "foo"})
+        mock.assert_called_with("POST",
+                                uri,
+                                data = {
+                                    'StatusCallback': 'http://www.example.com'
+                                },
+                                auth=self.auth,
+                                use_json_extension=True)
 
-    def test_status_callback_url(self):
-        resource = PhoneNumbers(self.uri, self.auth)
-        resource.update_instance = Mock()
-        resource.update("SID", status_callback="foo")
-        resource.update_instance.assert_called_with(
-                "SID", {"status_callback": "foo"})
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_application_sid(self, mock):
+        resp = create_mock_json('tests/resources/'
+                                'incoming_phone_numbers_instance.json')
+        resp.status_code = 201
+        mock.return_value = resp
 
-    def test_transfer(self):
-        resource = PhoneNumbers(self.uri, self.auth)
-        resource.update = Mock()
-        resource.transfer("SID", "AC123")
-        resource.update.assert_called_with("SID", account_sid="AC123")
+        self.list.update(self.sid, application_sid="foo")
 
-    def test_instance_transfer(self):
-        mock = Mock()
-        mock.uri = "/base"
-        instance = PhoneNumber(mock, "SID")
-        instance.transfer("AC123")
-        mock.transfer.assert_called_with("SID", "AC123")
+        uri = '%s/IncomingPhoneNumbers/%s' % (self.base_uri, self.sid)
+        data = {
+            "VoiceApplicationSid": "foo",
+            "SmsApplicationSid": "foo"
+        }
+        mock.assert_called_with("POST", uri, data=data, auth=self.auth,
+                                use_json_extension=True)
 
-    def test_base_uri(self):
-        parent = Mock()
-        parent.base_uri = ("https://api.twilio.com/2010-04-01/Accounts/"
-            "AC123")
-        resource = PhoneNumber(parent, "PNd2ae06cced59a5733d2c1c1c69a83a28")
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_update_with_multiple_params(self, mock):
+        resp = create_mock_json('tests/resources/'
+                                'incoming_phone_numbers_instance.json')
+        resp.status_code = 201
+        mock.return_value = resp
 
-        with open("tests/resources/incoming_phone_numbers_instance.json") as f:
-            entry = json.load(f)
-            resource.load(entry)
+        self.list.update(self.sid,
+                         voice_application_sid="a",
+                         sms_application_sid="b",
+                         status_callback="c")
 
-        self.assertEquals(resource.parent.base_uri,
-            ("https://api.twilio.com/2010-04-01/Accounts/AC4bf2dafbed59a573"
-             "3d2c1c1c69a83a28"))
+        uri = '%s/IncomingPhoneNumbers/%s' % (self.base_uri, self.sid)
+        data = {
+            "VoiceApplicationSid": "a",
+            "SmsApplicationSid": "b",
+            "StatusCallback": "c"
+        }
+        mock.assert_called_with("POST", uri, data=data, auth=self.auth,
+                                use_json_extension=True)
 
-    def test_purchase_type(self):
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_list_transfer(self, mock):
+        resp = create_mock_json('tests/resources/'
+                                'incoming_phone_numbers_instance.json')
+        resp.status_code = 201
+        mock.return_value = resp
+
+        self.list.transfer(self.sid, 'other_account')
+
+        uri = '%s/IncomingPhoneNumbers/%s' % (self.base_uri, self.sid)
+        data = {
+            "AccountSid": "other_account"
+        }
+        mock.assert_called_with("POST", uri, data=data, auth=self.auth,
+                                use_json_extension=True)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_list_transfer(self, mock):
+        resp = create_mock_json('tests/resources/'
+                                'incoming_phone_numbers_instance.json')
+        resp.status_code = 201
+        mock.return_value = resp
+
+        self.instance.transfer('other_account')
+
+        uri = '%s/IncomingPhoneNumbers/%s' % (self.base_uri, self.sid)
+        data = {
+            "AccountSid": "other_account"
+        }
+        mock.assert_called_with("POST", uri, data=data, auth=self.auth,
+                                use_json_extension=True)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_purchase_type(self, mock):
+        resp = create_mock_json('tests/resources/'
+                                'incoming_phone_numbers_instance.json')
+        resp.status_code = 201
+        mock.return_value = resp
 
         types = {'local': 'Local', 'mobile': 'Mobile', 'tollfree': 'TollFree'}
         for type in ('local', 'mobile', 'tollfree'):
-            resource = PhoneNumbers(self.uri, self.auth)
-            resource.request = Mock()
-            resource.request.return_value = (None, None)
-            resource.load_instance = Mock()
-            resource.purchase(type=type, phone_number='888')
-            resource.request.assert_called_with('POST', self.uri + '/IncomingPhoneNumbers/' + types[type],
-                                                data={'PhoneNumber': '888'})
+
+            self.list.purchase(type=type, phone_number='888', )
+            uri = '%s/IncomingPhoneNumbers/%s' % (self.base_uri, types[type])
+            data = {
+                'PhoneNumber': '888'
+            }
+            mock.assert_called_with("POST", uri, data=data, auth=self.auth,
+                                    use_json_extension=True)
 
 
 class IncomingPhoneNumbersTest(unittest.TestCase):
