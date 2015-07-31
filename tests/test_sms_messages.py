075368b41c2f176d4d9e4cc3b2d4ec15b0a548da
Ragil Prasetya <rprasetya@twilio.com>
2015-07-31 11:06:31 -0700

    Force sms to use generate code path

--- tests/test_sms_messages.py
+++ tests/test_sms_messages.py
@@ -1,38 +1,57 @@
 from datetime import date
 import unittest
 
-from mock import Mock
+from mock import patch
+from tests.tools import create_mock_json
 
 from twilio.rest.resources import SmsMessages
-
-DEFAULT = {
-    'From': None,
-    'DateSent<': None,
-    'DateSent>': None,
-    'DateSent': None,
-}
 
 
 class SmsTest(unittest.TestCase):
 
     def setUp(self):
-        self.resource = SmsMessages("foo", ("sid", "token"))
-        self.params = DEFAULT.copy()
+        self.auth = ("sid", "token")
+        self.resource = SmsMessages("foo", self.auth)
+        self.params = {}
 
-    def test_list_on(self):
-        self.resource.get_instances = Mock()
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_list_on(self, mock):
+        resp = create_mock_json("tests/resources/sms_messages_list.json")
+        resp.status_code = 201
+        mock.return_value = resp
+
         self.resource.list(date_sent=date(2011, 1, 1))
-        self.params['DateSent'] = "2011-01-01"
-        self.resource.get_instances.assert_called_with(self.params)
 
-    def test_list_after(self):
-        self.resource.get_instances = Mock()
+        self.params['DateSent'] = '2011-01-01'
+        mock.assert_called_with("GET", 'foo/SMS/Messages',
+                                params=self.params,
+                                auth=self.auth,
+                                use_json_extension=True)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_list_after(self, mock):
+        resp = create_mock_json("tests/resources/sms_messages_list.json")
+        resp.status_code = 201
+        mock.return_value = resp
+
         self.resource.list(after=date(2011, 1, 1))
-        self.params['DateSent>'] = "2011-01-01"
-        self.resource.get_instances.assert_called_with(self.params)
 
-    def test_list_before(self):
-        self.resource.get_instances = Mock()
+        self.params['DateSent>'] = '2011-01-01'
+        mock.assert_called_with("GET", 'foo/SMS/Messages',
+                                params=self.params,
+                                auth=self.auth,
+                                use_json_extension=True)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_list_before(self, mock):
+        resp = create_mock_json("tests/resources/sms_messages_list.json")
+        resp.status_code = 201
+        mock.return_value = resp
+
         self.resource.list(before=date(2011, 1, 1))
-        self.params['DateSent<'] = "2011-01-01"
-        self.resource.get_instances.assert_called_with(self.params)
+
+        self.params['DateSent<'] = '2011-01-01'
+        mock.assert_called_with("GET", 'foo/SMS/Messages',
+                                params=self.params,
+                                auth=self.auth,
+                                use_json_extension=True)
