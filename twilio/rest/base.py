16e1d51c5ba5b5a0991b04ae710efbb0a6cfb77e
matt <matt@twilio.com>
2015-09-24 09:04:47 -0700

    Correctly detect limits, makes read results replayable

--- twilio/rest/base.py
+++ twilio/rest/base.py
@@ -139,6 +139,11 @@ class Version(object):
         }
 
     def read(self, pager, instance, instance_kwargs, method, uri, limit, page_limit, **kwargs):
+        return Replayable(
+            self._read(pager, instance, instance_kwargs, method, uri, limit, page_limit, **kwargs)
+        )
+
+    def _read(self, pager, instance, instance_kwargs, method, uri, limit, page_limit, **kwargs):
         current_record = 1
         current_page = 1
         page = self.page(pager, instance, instance_kwargs, method, uri, **kwargs)
@@ -157,7 +162,7 @@ class Version(object):
             for record in page:
                 yield record
                 current_record += 1
-                if limit <= current_record:
+                if limit and limit <= current_record:
                     break
 
             current_page += 1
@@ -218,6 +223,24 @@ class Version(object):
 
         payload = json.loads(response.content)
         return instance(self, payload)
+
+
+class Replayable(object):
+    def __init__(self, generator):
+        self.generator = generator
+        self.cache = []
+        self.cached = False
+
+    def __iter__(self):
+        if self.cached:
+            for c in self.cache:
+                yield c
+        else:
+            for g in self.generator:
+                self.cache.append(g)
+                yield g
+            else:
+                self.cached = True
 
 
 class ListResource(object):
