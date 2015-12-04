d5c068075fd89104ff416cc573e66191eafd44c9
Senthil Ramakrishnan <senthgit@gmail.com>
2015-12-03 16:46:17 -0800

    Added TwilioConversationsClient and TwilioIpMessagingClient to _hush_pyflakes variable.

--- twilio/rest/__init__.py
+++ twilio/rest/__init__.py
@@ -7,6 +7,7 @@ from .pricing import TwilioPricingClient
 from .task_router import TwilioTaskRouterClient
 from .trunking import TwilioTrunkingClient
 
-_hush_pyflakes = [set_twilio_proxy, TwilioRestClient, TwilioLookupsClient,
-                  TwilioPricingClient, TwilioTaskRouterClient,
-                  TwilioTrunkingClient]
+_hush_pyflakes = [set_twilio_proxy, TwilioRestClient,
+                  TwilioConversationsClient, TwilioIpMessagingClient,
+                  TwilioLookupsClient, TwilioPricingClient,
+                  TwilioTaskRouterClient, TwilioTrunkingClient]
