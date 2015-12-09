625bd8a6c1c9b987cdcb39765083466afe0ad0a8
Senthil Ramakrishnan <senthgit@gmail.com>
2015-12-08 16:20:10 -0800

    Tests for UniqueName parameter

--- tests/ip_messaging/test_channels.py
+++ tests/ip_messaging/test_channels.py
@@ -20,9 +20,10 @@ class ChannelTest(unittest.TestCase):
         mock.return_value = resp
 
         uri = "%s/Channels" % (BASE_URI)
-        list_resource.create(friendly_name='TestChannel')
+        list_resource.create(friendly_name='TestChannel', unique_name='Unique')
         exp_params = {
-            'FriendlyName': "TestChannel"
+            'FriendlyName': "TestChannel",
+            'UniqueName': 'Unique'
         }
 
         mock.assert_called_with("POST", uri, data=exp_params, auth=AUTH,
@@ -34,10 +35,16 @@ class ChannelTest(unittest.TestCase):
         mock.return_value = resp
 
         uri = "%s/Channels/%s" % (BASE_URI, CHANNEL_SID)
-        list_resource.get(CHANNEL_SID)
+        channel_instance = list_resource.get(CHANNEL_SID)
 
         mock.assert_called_with("GET", uri, auth=AUTH,
                                 use_json_extension=False)
+        self.assertIsNotNone(channel_instance)
+        self.assertEquals('CHaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', channel_instance.sid)
+        self.assertEquals('ACaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', channel_instance.account_sid)
+        self.assertEquals('ISaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', channel_instance.service_sid)
+        self.assertEquals('update', channel_instance.friendly_name)
+        self.assertEquals('unique', channel_instance.unique_name)
 
     @patch("twilio.rest.resources.base.Resource.request")
     def test_delete(self, req):
