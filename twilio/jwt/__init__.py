05133ff28d3e982f7b917fb65196d82a839416ad
Evan Fossier <evan.fossier@gmail.com>
2017-02-20 13:44:49 -0800

    Fix python3.2 failing jwt tests

--- twilio/jwt/__init__.py
+++ twilio/jwt/__init__.py
@@ -1,3 +1,12 @@
+import hmac
+import sys
+
+from twilio.jwt import compat
+
+if sys.version_info[0] == 3 and sys.version_info[1] == 2:
+    # PyJWT expects hmac.compare_digest to exist even under python 3.2
+    hmac.compare_digest = compat.compare_digest
+
 import jwt as jwt_lib
 
 try:
@@ -140,7 +149,7 @@ class Jwt(object):
         verify = True if key else False
 
         try:
-            payload = jwt_lib.decode(jwt, key, verify=verify, options={
+            payload = jwt_lib.decode(bytes(jwt), key, verify=verify, options={
                 'verify_signature': True,
                 'verify_exp': True,
                 'verify_nbf': True,
