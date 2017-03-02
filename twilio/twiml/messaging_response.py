b84ed80355e48bb348db53ebdd1e38c4f06f1873
Jingming Niu <niu@jingming.ca>
2017-03-02 14:57:22 -0800

    Add docs

--- twilio/twiml/messaging_response.py
+++ twilio/twiml/messaging_response.py
@@ -2,8 +2,13 @@ from twilio.twiml import TwiML
 
 
 class MessagingResponse(TwiML):
-
+    """
+    Messaging TwiML Response
+    """
     def __init__(self):
+        """
+        Create a new <Response>
+        """
         super(MessagingResponse, self).__init__()
         self.name = 'Response'
 
@@ -15,6 +20,18 @@ class MessagingResponse(TwiML):
                 action=None,
                 status_callback=None,
                 **kwargs):
+        """
+        Add a <Message> element
+
+        :param body: body of the message
+        :param to: number to send to
+        :param from_: number to send from
+        :param method: action HTTP method
+        :param action: action URL
+        :param status_callback: callback URL
+        :param kwargs: other attributes
+        :return: <Message> element
+        """
         return self.append(Message(
             body=body,
             to=to,
@@ -26,6 +43,14 @@ class MessagingResponse(TwiML):
         ))
 
     def redirect(self, method=None, url=None, **kwargs):
+        """
+        Add a <Redirect> element
+
+        :param method: HTTP method
+        :param url: URL to redirect to
+        :param kwargs: other attributes
+        :return: <Redirect> element
+        """
         return self.append(Redirect(
             method=method,
             url=url,
@@ -34,31 +59,70 @@ class MessagingResponse(TwiML):
 
 
 class Message(TwiML):
-
+    """
+    <Message> element
+    """
     def __init__(self, body=None, **kwargs):
+        """
+        Create a new <Message> element
+
+        :param body: body of message
+        :param kwargs: other attributes
+        """
         super(Message, self).__init__(**kwargs)
         if body:
             self.body = body
 
     def body(self, body):
+        """
+        Add a <Body> element
+
+        :param body: body of message
+        :return: <Body> element
+        """
         return self.append(Body(body))
 
     def media(self, url):
+        """
+        Add a <Media> element
+
+        :param url: media URL
+        :return: <Media> element
+        """
         return self.append(Media(url))
 
 
 class Body(TwiML):
+    """
+    <Body> element
+    """
     def __init__(self, body):
+        """
+        Create a new <Body> element
+
+        :param body: message body
+        """
         super(Body, self).__init__()
         self.body = body
 
 
 class Media(TwiML):
+    """
+    <Media> element
+    """
     def __init__(self, url):
+        """
+        Create a new <Media> element
+
+        :param url: media URL location
+        """
         super(Media, self).__init__()
         self.body = url
 
 
 class Redirect(TwiML):
+    """
+    <Redirect> element
+    """
     pass
 
--- twilio/twiml/voice_response.py
+++ twilio/twiml/voice_response.py
@@ -2,8 +2,13 @@ from twilio.twiml import TwiML
 
 
 class VoiceResponse(TwiML):
-
+    """
+    Voice TwiML Response
+    """
     def __init__(self):
+        """
+        Create a new <Response>
+        """
         super(VoiceResponse, self).__init__()
         self.name = 'Response'
 
@@ -20,6 +25,23 @@ class VoiceResponse(TwiML):
              recording_status_callback=None,
              recording_status_callback_method=None,
              **kwargs):
+        """
+        Create a <Dial> element
+
+        :param number: phone number to dial
+        :param action: action URL
+        :param method: action HTTP method
+        :param timeout: time to wait for answer
+        :param hangup_on_star: hangup call on * press
+        :param time_limit: max time length
+        :param caller_id: caller ID to display
+        :param record: record the call
+        :param trim: trim the recording
+        :param recording_status_callback: status callback URL
+        :param recording_status_callback_method: status callback URL method
+        :param kwargs: additional attributes
+        :return: <Dial> element
+        """
         return self.append(Dial(
             number=number,
             action=action,
@@ -43,6 +65,18 @@ class VoiceResponse(TwiML):
                 wait_url_method=None,
                 workflow_sid=None,
                 **kwargs):
+        """
+        Add a new <Enqueue> element
+
+        :param name: friendly name
+        :param action: action URL
+        :param method: action URL method
+        :param wait_url: wait URL
+        :param wait_url_method: wait URL method
+        :param workflow_sid: TaskRouter workflow SID
+        :param kwargs: additional attributes
+        :return: <Enqueue> element
+        """
         return self.append(Enqueue(
             name,
             action=action,
@@ -60,6 +94,17 @@ class VoiceResponse(TwiML):
                finish_on_key=None,
                num_digits=None,
                **kwargs):
+        """
+        Add a new <Gather> element
+
+        :param action: action URL
+        :param method: action URL method
+        :param timeout: time to wait while gathering input
+        :param finish_on_key: finish on key press
+        :param num_digits: digits to collect
+        :param kwargs: additional attributes
+        :return: <Gather> element
+        """
         return self.append(Gather(
             action=action,
             method=method,
@@ -69,12 +114,28 @@ class VoiceResponse(TwiML):
         ))
 
     def hangup(self):
+        """
+        Add a new <Hangup> element
+
+        :return: <Hangup> element
+        """
         return self.append(Hangup())
 
     def leave(self):
+        """
+        Add a new <Leave> element
+
+        :return: <Leave> element
+        """
         return self.append(Leave())
 
     def pause(self, length=None):
+        """
+        Add a new <Pause> element
+
+        :param length: time in seconds to pause
+        :return: <Pause> element
+        """
         return self.append(Pause(length=length))
 
     def play(self,
@@ -82,6 +143,15 @@ class VoiceResponse(TwiML):
              loop=None,
              digits=None,
              **kwargs):
+        """
+        Add a new <Play> element
+
+        :param url: url to play
+        :param loop: times to loop
+        :param digits: play DTMF tones during a call
+        :param kwargs: additional attributes
+        :return: <Play> element
+        """
         return self.append(Play(
             url,
             loop=loop,
@@ -102,6 +172,23 @@ class VoiceResponse(TwiML):
                transcribe=None,
                transcribe_callback=None,
                **kwargs):
+        """
+        Add a new <Record> element
+
+        :param action: action URL
+        :param method: action URL method
+        :param timeout: timeout for recording
+        :param finish_on_key: finish recording on key
+        :param max_length: max length to record
+        :param play_beep: play beep
+        :param trim: trim the recording
+        :param recording_status_callback: status callback for the recordings
+        :param recording_status_callback_method: status callback method
+        :param transcribe: transcribe the recording
+        :param transcribe_callback: transcribe callback URL
+        :param kwargs: additional attributes
+        :return: <Record> element
+        """
         return self.append(Record(
             action=action,
             method=method,
@@ -118,9 +205,24 @@ class VoiceResponse(TwiML):
         ))
 
     def redirect(self, url, method=None, **kwargs):
+        """
+        Add a <Redirect> element
+
+        :param url: redirect url
+        :param method: redirect method
+        :param kwargs: additional attributes
+        :return: <Redirect> element
+        """
         return self.append(Redirect(url, method=method, **kwargs))
 
     def reject(self, reason=None, **kwargs):
+        """
+        Add a <Reject> element
+
+        :param reason: rejection reason
+        :param kwargs: additional attributes
+        :return: <Reject> element
+        """
         return self.append(Reject(reason=reason, **kwargs))
 
     def say(self,
@@ -129,6 +231,16 @@ class VoiceResponse(TwiML):
             language=None,
             voice=None,
             **kwargs):
+        """
+        Add a <Say> element
+
+        :param body: message body
+        :param loop: times to loop
+        :param language: language of message
+        :param voice: voice to use
+        :param kwargs: additional attributes
+        :return: <Say> element
+        """
         return self.append(Say(
             body,
             loop=loop,
@@ -145,6 +257,18 @@ class VoiceResponse(TwiML):
             action=None,
             status_callback=None,
             **kwargs):
+        """
+        Add a <Sms> element
+
+        :param body: body of message
+        :param to: to phone number
+        :param from_: from phone number
+        :param method: action URL method
+        :param action: action URL
+        :param status_callback: status callback URL
+        :param kwargs: additional attributes
+        :return: <Sms> element
+        """
         return self.append(Sms(
             body,
             to=to,
@@ -157,7 +281,16 @@ class VoiceResponse(TwiML):
 
 
 class Dial(TwiML):
+    """
+    <Dial> element
+    """
     def __init__(self, number=None, **kwargs):
+        """
+        Create a new <Dial> element
+
+        :param number: phone number to dial
+        :param kwargs: additional attributes
+        """
         super(Dial, self).__init__(**kwargs)
         if number:
             self.body = number
@@ -170,6 +303,18 @@ class Dial(TwiML):
                status_callback_method=None,
                status_callback=None,
                **kwargs):
+        """
+        Add a new <Client> element
+
+        :param name: name of client
+        :param method: action URL method
+        :param url: action URL
+        :param status_callback_event: events to call status callback
+        :param status_callback_method: status callback URL method
+        :param status_callback: status callback URL
+        :param kwargs: additional attributes
+        :return: <Client> element
+        """
         return self.append(Client(
             name,
             method=method,
@@ -198,6 +343,28 @@ class Dial(TwiML):
                    recording_status_callback=None,
                    recording_status_callback_method=None,
                    **kwargs):
+        """
+        Add a <Conference> element
+
+        :param name: name of conference
+        :param muted: join the conference muted
+        :param start_conference_on_enter: start the conference on enter
+        :param end_conference_on_exit: end the conference on exit
+        :param max_participants: max number of people in conference
+        :param beep: play beep when joining
+        :param record: record the conference
+        :param trim: trim the recording
+        :param wait_method: wait URL method
+        :param wait_url: wait URL to play
+        :param event_callback_url: event callback URL
+        :param status_callback_event: events to call status callback
+        :param status_callback: status callback URL
+        :param status_callback_method: status callback URL method
+        :param recording_status_callback: recording status callback URL
+        :param recording_status_callback_method: recording status callback URL method
+        :param kwargs: additional attributes
+        :return: <Conference> element
+        """
         return self.append(Conference(
             name,
             start_conference_on_enter=start_conference_on_enter,
@@ -226,6 +393,19 @@ class Dial(TwiML):
                status_callback=None,
                status_callback_method=None,
                **kwargs):
+        """
+        Add a <Number> element
+
+        :param number: phone number to dial
+        :param send_digits: play DTMF tones when the call is answered
+        :param url: TwiML URL
+        :param method: TwiML URL method
+        :param status_callback_event: events to call status callback
+        :param status_callback: status callback URL
+        :param status_callback_method: status callback URL method
+        :param kwargs: additional attributes
+        :return: <Number> element
+        """
         return self.append(Number(
             number,
             send_digits=send_digits,
@@ -244,6 +424,17 @@ class Dial(TwiML):
               reservation_sid=None,
               post_work_activity_sid=None,
               **kwargs):
+        """
+        Add a <Queue> element
+
+        :param queue_name: queue name
+        :param url: action URL
+        :param method: action URL method
+        :param reservation_sid: TaskRouter reservation SID
+        :param post_work_activity_sid: TaskRouter activity SID
+        :param kwargs: additional attributes
+        :return: <Queue> element
+        """
         return self.append(Queue(
             queue_name,
             url=url,
@@ -263,6 +454,20 @@ class Dial(TwiML):
             status_callback=None,
             status_callback_method=None,
             **kwargs):
+        """
+        Add a <Sip> element
+
+        :param uri: sip url
+        :param username: sip username
+        :param password: sip password
+        :param url: action URL
+        :param method: action URL method
+        :param status_callback_event: events to call status callback
+        :param status_callback: status callback URL
+        :param status_callback_method: status callback URL method
+        :param kwargs: additional attributes
+        :return: <Sip> element
+        """
         return self.append(Sip(
             uri,
             username=username,
@@ -277,43 +482,104 @@ class Dial(TwiML):
 
 
 class Client(TwiML):
+    """
+    <Client> element
+    """
     def __init__(self, name, **kwargs):
+        """
+        Create a new <Client> element
+
+        :param name: name of client
+        :param kwargs: attributes
+        """
         super(Client, self).__init__(**kwargs)
         self.body = name
 
 
 class Conference(TwiML):
+    """
+    <Conference> element
+    """
     def __init__(self, name, **kwargs):
+        """
+        Create a new <Conference> element
+
+        :param name: name of conference
+        :param kwargs: attributes
+        """
         super(Conference, self).__init__(**kwargs)
         self.body = name
 
 
 class Number(TwiML):
+    """
+    <Number> element
+    """
     def __init__(self, number, **kwargs):
+        """
+        Create a new <Number> element
+
+        :param number: phone number
+        :param kwargs: attributes
+        """
         super(Number, self).__init__(**kwargs)
         self.body = number
 
 
 class Queue(TwiML):
+    """
+    <Queue> element
+    """
     def __init__(self, queue_name, **kwargs):
+        """
+        Create a new <Queue> element
+
+        :param queue_name: name of queue
+        :param kwargs: attributes
+        """
         super(Queue, self).__init__(**kwargs)
         self.body = queue_name
 
 
 class Sip(TwiML):
+    """
+    <Sip> element
+    """
     def __init__(self, uri, **kwargs):
+        """
+        Create a new <Sip> element
+
+        :param uri: sip url
+        :param kwargs: attributes
+        """
         super(Sip, self).__init__(**kwargs)
         self.body = uri
 
 
 class Enqueue(TwiML):
+    """
+    <Enqueue> element
+    """
     def __init__(self, name, **kwargs):
+        """
+        Create a new <Enqueue> element
+
+        :param name: queue name
+        :param kwargs: attributes
+        """
         super(Enqueue, self).__init__(**kwargs)
         self.body = name
 
 
 class Gather(TwiML):
+    """
+    <Gather> element
+    """
     def __init__(self, **kwargs):
+        """
+        Create a new <Gather> element
+        :param kwargs: attributes
+        """
         super(Gather, self).__init__(**kwargs)
 
     def say(self,
@@ -322,6 +588,16 @@ class Gather(TwiML):
             language=None,
             voice=None,
             **kwargs):
+        """
+        Add a new <Say> element
+
+        :param body: message body
+        :param loop: times to loop
+        :param language: message language
+        :param voice: voice to use
+        :param kwargs: additional attributes
+        :return: <Say> element
+        """
         return self.append(Say(
             body,
             loop=loop,
@@ -335,6 +611,15 @@ class Gather(TwiML):
              loop=None,
              digits=None,
              **kwargs):
+        """
+        Add a new <Play> element
+
+        :param url: media URL
+        :param loop: times to loop
+        :param digits: digits to simulate
+        :param kwargs: additional attributes
+        :return: <Play> element
+        """
         return self.append(Play(
             url,
             loop=loop,
@@ -343,48 +628,105 @@ class Gather(TwiML):
         ))
 
     def pause(self, length=None):
+        """
+        Add a new <Pause> element
+
+        :param length: time to pause
+        :return: <Pause> element
+        """
         return self.append(Pause(length=length))
 
 
 class Pause(TwiML):
+    """
+    <Pause> element
+    """
     pass
 
 
 class Play(TwiML):
+    """
+    <Play> element
+    """
     def __init__(self, url, **kwargs):
+        """
+        Create a new <Play> element
+
+        :param url: media URL
+        :param kwargs: additional attributes
+        """
         super(Play, self).__init__(**kwargs)
         self.body = url
 
 
 class Say(TwiML):
+    """
+    <Say> element
+    """
     def __init__(self, body, **kwargs):
+        """
+        Create a new <Say> element
+
+        :param body: message body
+        :param kwargs: attributes
+        """
         super(Say, self).__init__(**kwargs)
         self.body = body
 
 
 class Hangup(TwiML):
+    """
+    <Hangup> element
+    """
     pass
 
 
 class Leave(TwiML):
+    """
+    <Leave> element
+    """
     pass
 
 
 class Record(TwiML):
+    """
+    <Record> element
+    """
     pass
 
 
 class Redirect(TwiML):
+    """
+    <Redirect> element
+    """
     def __init__(self, url, **kwargs):
+        """
+        Create a new <Redirect> element
+
+        :param url: TwiML URL
+        :param kwargs: attributes
+        """
         super(Redirect, self).__init__(**kwargs)
         self.body = url
 
 
 class Reject(TwiML):
+    """
+    <Reject> element
+    """
     pass
 
 
 class Sms(TwiML):
+    """
+    <Sms> element
+    """
     def __init__(self, body, **kwargs):
+        """
+        Create a new <Sms> element
+
+        :param body: message body
+        :param kwargs: attributes
+        """
         super(Sms, self).__init__(**kwargs)
         self.body = body
