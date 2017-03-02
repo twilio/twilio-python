dc51b11d4f65ff551c5353914b7f756da91fbaa3
Doug Black <dblack@twilio.com>
2017-03-02 13:35:20 -0800

    Document `http/__init__.py`

--- twilio/http/__init__.py
+++ twilio/http/__init__.py
@@ -20,13 +20,12 @@ def get_cert_file():
 
 
 class HttpClient(object):
-    def request(self,
-                method,
-                url,
-                params=None,
-                data=None,
-                headers=None,
-                auth=None,
-                timeout=None,
-                allow_redirects=False):
+    """
+    An abstract class representing an HTTP client.
+    """
+    def request(self, method, url, params=None, data=None, headers=None, auth=None,
+                timeout=None, allow_redirects=False):
+        """
+        Make an HTTP request.
+        """
         raise TwilioException('HttpClient is an abstract class')
