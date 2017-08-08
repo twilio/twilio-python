7e80f7034ba4e4db21ebf970c4cd84ba9bd90777
Tyson Holub <tholub@mobiusworks.com>
2017-08-08 13:49:40 -0400

    add request logging and hooking

--- twilio/http/http_client.py
+++ twilio/http/http_client.py
@@ -1,18 +1,27 @@
-from requests import Request, Session
+from requests import Request, Session, hooks
 
 from twilio.http import HttpClient
 from twilio.http.response import Response
 from twilio.http.request import Request as TwilioRequest
+import logging
+import json
+try:
+    from urllib.parse import urlencode
+except ImportError:
+    from urllib import urlencode
+
+_logger = logging.getLogger('twilio.http_client')
 
 
 class TwilioHttpClient(HttpClient):
     """
     General purpose HTTP Client for interacting with the Twilio API
     """
-    def __init__(self, pool_connections=True):
+    def __init__(self, pool_connections=True, request_hooks=None):
         self.session = Session() if pool_connections else None
         self.last_request = None
         self.last_response = None
+        self.request_hooks = request_hooks or hooks.default_hooks()
 
     def request(self, method, url, params=None, data=None, headers=None, auth=None, timeout=None,
                 allow_redirects=False):
@@ -33,9 +42,27 @@ class TwilioHttpClient(HttpClient):
         :rtype: A :class:`Response <twilio.rest.http.response.Response>` object
         """
 
+        kwargs = dict(
+            method=method.upper(),
+            url=url,
+            params=params,
+            data=data,
+            headers=headers,
+            auth=auth,
+            hooks=self.request_hooks
+        )
+
+        if params:
+            _logger.info('{method} Request: {url}?{query}'.format(query=urlencode(params), **kwargs))
+            _logger.info('PARAMS: {params}'.format(**kwargs))
+        else:
+            _logger.info('{method} Request: {url}'.format(**kwargs))
+        if data:
+            _logger.info('PAYLOAD: {data}'.format(**kwargs))
+
         self.last_response = None
         session = self.session or Session()
-        request = Request(method.upper(), url, params=params, data=data, headers=headers, auth=auth)
+        request = Request(**kwargs)
         self.last_request = TwilioRequest(method.upper(), url, auth=auth, params=params, data=data,
                                           headers=headers)
 
@@ -45,6 +72,8 @@ class TwilioHttpClient(HttpClient):
             allow_redirects=allow_redirects,
             timeout=timeout,
         )
+
+        _logger.info('{method} Response: {status} {text}'.format(method=method, status=response.status_code, text=response.text))
 
         self.last_response = Response(int(response.status_code), response.text)
 
