22eb9440e9504ad7e4a55a3752d383ee4e97c121
Tyson Holub <tysonholub@gmail.com>
2017-08-09 15:53:19 -0400

    Update http_client.py

--- twilio/http/http_client.py
+++ twilio/http/http_client.py
@@ -7,7 +7,7 @@ import json
 try:
     from urllib.parse import urlencode
 except ImportError:
-    from urlparse import urlencode
+    from urllib import urlencode
 
 _logger = logging.getLogger('twilio.http_client')
 
