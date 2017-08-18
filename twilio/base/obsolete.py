1bf8807e73242decd8af15d8b5e9ddce13c748e5
Doug Black <dblack@twilio.com>
2017-08-18 12:32:51 -0700

    Fix spacing

--- twilio/base/obsolete.py
+++ twilio/base/obsolete.py
@@ -8,14 +8,16 @@ class ObsoleteException(BaseException):
 
 
 def obsolete_client(func):
-    '''This is a decorator which can be used to mark Client classes
-    as obsolete. It will result in an error being emitted
-    when the class is instantiated.'''
+    """This is a decorator which can be used to mark Client classes as
+    obsolete. It will result in an error being emitted when the class is
+    instantiated."""
 
     @functools.wraps(func)
     def new_func(*args, **kwargs):
-        raise ObsoleteException("{} has been removed from this version of the library. "
-                              "Please refer to current documentation for guidance."
-                              .format(func.__name__))
+        raise ObsoleteException(
+            "{} has been removed from this version of the library. "
+            "Please refer to current documentation for guidance."
+            .format(func.__name__)
+        )
 
     return new_func
