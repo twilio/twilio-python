da444a2b3b767791c47b453dc3613f503617be10
Evan Fossier <efossier@twilio.com>
2017-12-12 10:07:35 -0800

    Only lower camel twiml kwargs if they contain _

--- tests/unit/twiml/__init__.py
+++ tests/unit/twiml/__init__.py
@@ -3,7 +3,12 @@ import unittest
 from nose.tools import raises
 from six import text_type
 
-from twilio.twiml import format_language, TwiMLException, TwiML
+from twilio.twiml import (
+    format_language,
+    lower_camel,
+    TwiMLException,
+    TwiML
+)
 
 
 class TwilioTest(unittest.TestCase):
@@ -30,3 +35,24 @@ class TwilioTest(unittest.TestCase):
     @raises(TwiMLException)
     def test_format_language_fail(self):
         format_language('this is invalid')
+
+    def test_lower_camel_empty_string(self):
+        self.assertEqual('', lower_camel(''))
+
+    def test_lower_camel_none(self):
+        self.assertEqual(None, lower_camel(None))
+
+    def test_lower_camel_single_word(self):
+        self.assertEqual('foo', lower_camel('foo'))
+
+    def test_lower_camel_double_word(self):
+        self.assertEqual('fooBar', lower_camel('foo_bar'))
+
+    def test_lower_camel_multi_word(self):
+        self.assertEqual('fooBarBaz', lower_camel('foo_bar_baz'))
+
+    def test_lower_camel_multi_word_mixed_case(self):
+        self.assertEqual('fooBarBaz', lower_camel('foO_Bar_baz'))
+
+    def test_lower_camel_camel_cased(self):
+        self.assertEqual('fooBar', lower_camel('fooBar'))
