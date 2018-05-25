429e53a233518f49b3bbe2edbf29e96e7689f2f9
cjcodes <chris@cjcodes.com>
2018-05-25 14:01:10 -0400

    Migrate readme to rst and load it in with setup.py (#417)
    * Migrate readme to rst and load it in with setup.py
    
    * Add images back and convert smartquotes back

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ __version__ = None
 with open('twilio/__init__.py') as f:
     exec(f.read())
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 # To install the twilio-python library, open a Terminal shell, then run this
 # file by typing:
 #
@@ -51,14 +54,5 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Communications :: Telephony",
         ],
-    long_description = """\
-    Python Twilio Helper Library
-    ----------------------------
-
-    DESCRIPTION
-    The Twilio REST SDK simplifies the process of making calls using the Twilio REST API.
-    The Twilio REST API lets to you initiate outgoing calls, list previous calls,
-    and much more.  See https://www.github.com/twilio/twilio-python for more information.
-
-     LICENSE The Twilio Python Helper Library is distributed under the MIT
-    License """ )
+    long_description = long_description
+)
