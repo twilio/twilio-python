ca9e6abe7f72570c9a77865ab85f7dcb965f7ee6
Ashish Sharma <asharma@twilio.com>
2020-04-27 12:43:58 -0700

    default detail to none and add param docs

--- twilio/base/exceptions.py
+++ twilio/base/exceptions.py
@@ -17,9 +17,10 @@ class TwilioRestException(TwilioException):
     :param str method: The HTTP method used to make the request
     :param int|None code: A Twilio-specific error code for the error. This is
          not available for all errors.
+    :param dictionary|None details: Additional error details returned for the exception
     """
 
-    def __init__(self, status, uri, msg="", code=None, method='GET', details={}):
+    def __init__(self, status, uri, msg="", code=None, method='GET', details=None):
         self.uri = uri
         self.status = status
         self.msg = msg
