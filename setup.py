85706b3942a0da8831c6653ec1af1278b066a958
Jennifer Mah <42650198+JenniferMah@users.noreply.github.com>
2022-12-09 11:40:19 -0800

    chore: add support for python 3.11 (#627)

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Communications :: Telephony",
