1a587eea13da2e6de77cc4f048fd284d547b98fd
Claudia Chua <43133161+claudiachua@users.noreply.github.com>
2023-01-12 12:04:27 -0700

    chore: refactor client (#634)

--- tests/unit/rest/test_client.py
+++ tests/unit/rest/test_client.py
@@ -1,53 +1,8 @@
 import unittest
-import platform
 
-from twilio import __version__
 from twilio.rest import (
-    Client,
-    TwilioClient,
-    TwilioRestClient,
-    TwilioIpMessagingClient,
-    TwilioLookupsClient,
-    TwilioMonitorClient,
-    TwilioPricingClient,
-    TwilioTaskRouterClient,
-    TwilioTrunkingClient,
+    Client
 )
-from twilio.base.obsolete import ObsoleteException
-
-
-class TestDummyClients(unittest.TestCase):
-    def test_obsolete_exception_twilioclient(self):
-        self.assertRaises(ObsoleteException, TwilioClient,
-                          "Expected raised ObsoleteException")
-
-    def test_obsolete_exception_twiliorestclient(self):
-        self.assertRaises(ObsoleteException, TwilioRestClient,
-                          "Expected raised ObsoleteException")
-
-    def test_obsolete_exception_twilioipmessagingclient(self):
-        self.assertRaises(ObsoleteException, TwilioIpMessagingClient,
-                          "Expected raised ObsoleteException")
-
-    def test_obsolete_exception_twiliolookupsclient(self):
-        self.assertRaises(ObsoleteException, TwilioLookupsClient,
-                          "Expected raised ObsoleteException")
-
-    def test_obsolete_exception_twiliomonitorclient(self):
-        self.assertRaises(ObsoleteException, TwilioMonitorClient,
-                          "Expected raised ObsoleteException")
-
-    def test_obsolete_exception_twiliopricingclient(self):
-        self.assertRaises(ObsoleteException, TwilioPricingClient,
-                          "Expected raised ObsoleteException")
-
-    def test_obsolete_exception_twiliotaskrouterclient(self):
-        self.assertRaises(ObsoleteException, TwilioTaskRouterClient,
-                          "Expected raised ObsoleteException")
-
-    def test_obsolete_exception_twiliotrunkingclient(self):
-        self.assertRaises(ObsoleteException, TwilioTrunkingClient,
-                          "Expected raised ObsoleteException")
 
 
 class TestRegionEdgeClients(unittest.TestCase):
