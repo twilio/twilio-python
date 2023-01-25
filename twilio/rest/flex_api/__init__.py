45816bfe98209ae0d958e48033917281c3521eae
Claudia Chua <43133161+claudiachua@users.noreply.github.com>
2023-01-26 04:12:21 +0800

    fix: remove Flex shortcuts for removed APIs (#641)

--- twilio/rest/flex_api/__init__.py
+++ twilio/rest/flex_api/__init__.py
@@ -25,19 +25,9 @@ class FlexApi(FlexApiBase):
         return self.v1.flex_flow
 
     @property
-    def good_data(self):
-        warn('good_data() is deprecated. Use v1.good_data() instead.', DeprecationWarning, stacklevel=2)
-        return self.v1.good_data
-
-    @property
     def interaction(self):
         warn('interaction() is deprecated. Use v1.interaction() instead.', DeprecationWarning, stacklevel=2)
         return self.v1.interaction
-
-    @property
-    def user_roles(self):
-        warn('user_roles() is deprecated. Use v1.user_roles() instead.', DeprecationWarning, stacklevel=2)
-        return self.v1.user_roles
 
     @property
     def web_channel(self):
