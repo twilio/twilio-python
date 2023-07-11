3324a44a3e21fb138c53ef9f075a0ff1b6b1ff91
Athira Sabu <102021496+AsabuHere@users.noreply.github.com>
2023-07-11 16:45:15 +0530

    Update __init__.py

--- twilio/rest/intelligence/__init__.py
+++ twilio/rest/intelligence/__init__.py
@@ -1,6 +1,4 @@
 from warnings import warn
-
-
 from twilio.rest.intelligence.IntelligenceBase import IntelligenceBase
 from twilio.rest.intelligence.v2.service import ServiceList
 from twilio.rest.intelligence.v2.transcript import TranscriptList
