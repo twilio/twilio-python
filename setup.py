abe831537e4bab1d6f618aadc0e720fb37431087
Twilio <team_interfaces+github@twilio.com>
2024-01-25 11:46:10 +0000

    Release 8.12.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md") as f:
 
 setup(
     name="twilio",
-    version="8.11.1",
+    version="8.12.0",
     description="Twilio API client and TwiML generator",
     author="Twilio",
     help_center="https://www.twilio.com/help/contact",
--- twilio/__init__.py
+++ twilio/__init__.py
@@ -1,2 +1,2 @@
-__version_info__ = ("8", "11", "1")
+__version_info__ = ("8", "12", "0")
 __version__ = ".".join(__version_info__)
