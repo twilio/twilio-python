a1521dd9067f566a751fa1a9f2f3ab612ec25f9e
manisha1997 <singhmanisha.250025@gmail.com>
2024-07-09 18:20:41 +0530

    chore: oauth sdk implementation

--- twilio/http/token_manager.py
+++ twilio/http/token_manager.py
@@ -5,7 +5,7 @@ class TokenManager:
 
     @classmethod
     def init(cls, grant_type: str, client_id: str, client_secret: str, code: str = None, redirect_uri: str = None, audience: str = None, refreshToken: str = None, scope: str = None, tokenManager: OrgTokenManager = None):
-        if tokenManager is not None:
+        if tokenManager is None:
             cls.org_token_manager = OrgTokenManager(grant_type, client_id, client_secret, code, redirect_uri, audience, refreshToken, scope)
         else:
             cls.org_token_manager = tokenManager
