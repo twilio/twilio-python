66f3e2871cc2f6abef453e8dd16e354b0995bc61
AsabuHere <asabu@twilio.com>
2024-12-11 22:47:50 +0530

    Organization api uptake changes

--- twilio/rest/preview/__init__.py
+++ twilio/rest/preview/__init__.py
@@ -1,6 +1,7 @@
 from warnings import warn
 
 from twilio.rest.preview.PreviewBase import PreviewBase
+from twilio.rest.preview.deployed_devices.fleet import FleetList
 from twilio.rest.preview.hosted_numbers.authorization_document import (
     AuthorizationDocumentList,
 )
@@ -14,6 +15,14 @@ from twilio.rest.preview.wireless.sim import SimList
 
 
 class Preview(PreviewBase):
+    @property
+    def fleets(self) -> FleetList:
+        warn(
+            "fleets is deprecated. Use deployed_devices.fleets instead.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self.deployed_devices.fleets
 
 
     @property
