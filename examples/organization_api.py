0b8aab400793efc06c1beb4a6a0f931b21df0ca1
Athira Sabu <102021496+AsabuHere@users.noreply.github.com>
2024-12-12 16:01:56 +0530

    feat: Docs update and examples for organization api uptake and public oauth (#825)
    * Docs update and examples

---
+++ examples/organization_api.py
@@ -0,0 +1,30 @@
+import os
+
+from twilio.rest import Client
+from twilio.credential.orgs_credential_provider import OrgsCredentialProvider
+
+ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+API_KEY = os.environ.get("TWILIO_API_KEY")
+API_SECRET = os.environ.get("TWILIO_API_SECRET")
+
+CLIENT_ID = os.environ.get("TWILIO_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+ORGS_SID = os.environ.get("ORGS_SID")
+
+
+def example():
+    """
+    Some example usage of using organization resources
+    """
+    self.client = Client(
+        account_sid=ACCOUNT_SID,
+        credential_provider= OrgsCredentialProvider(CLIENT_ID,CLIENT_SECRET)
+    )
+
+    accounts = self.client.preview_iam.organization(organization_sid=ORGS_SID).accounts.stream()
+    for record in accounts:
+        print(record)
+
+
+if __name__ == "__main__":
+    example()
---
+++ examples/public_oauth.py
@@ -0,0 +1,31 @@
+import os
+
+from twilio.rest import Client
+from twilio.credential.client_credential_provider import ClientCredentialProvider
+
+ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+API_KEY = os.environ.get("TWILIO_API_KEY")
+API_SECRET = os.environ.get("TWILIO_API_SECRET")
+FROM_NUMBER = os.environ.get("TWILIO_FROM_NUMBER")
+TO_NUMBER = os.environ.get("TWILIO_TO_NUMBER")
+
+CLIENT_ID = os.environ.get("TWILIO_CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+
+
+def example():
+    """
+    Some example usage of message resources.
+    """
+    self.client = Client(
+        account_sid=ACCOUNT_SID,
+        credential_provider= ClientCredentialProvider(CLIENT_ID,CLIENT_SECRET)
+    )
+
+    msg = self.client.messages.create(
+        to=self.to_number, from_=self.from_number, body="hello world"
+    )
+
+
+if __name__ == "__main__":
+    example()
