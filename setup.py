20edf3aca1975bc4b8deb58971c653c24ab3c4cc
Shubham Tiwari <stiwari@twilio.com>
2025-01-24 10:14:08 +0530

    chore: enable newer versions of aiohttp-retry

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
         "requests >= 2.0.0",
         "PyJWT >= 2.0.0, < 3.0.0",
         "aiohttp>=3.8.4",
-        "aiohttp-retry==2.8.3",
+        "aiohttp-retry>=2.8.3",
     ],
     packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
