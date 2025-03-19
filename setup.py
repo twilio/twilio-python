937fda2755e5c88ef332cf6b0467b6e934aae491
Anders Kaseorg <andersk@mit.edu>
2025-03-19 13:52:15 -0700

    fix: SetuptoolsDeprecationWarning: License classifiers are deprecated.
    /home/anders/.cache/uv/builds-v0/.tmpXAxITl/lib/python3.12/site-packages/setuptools/dist.py:751: SetuptoolsDeprecationWarning: License classifiers are deprecated.
    !!
    
            ********************************************************************************
            Please consider removing the following classifiers in favor of a SPDX license expression:
    
            License :: OSI Approved :: MIT License
    
            See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
            ********************************************************************************
    
    !!
      self._finalize_license_expression()
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
