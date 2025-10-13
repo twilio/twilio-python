2aa9e67c21538a9919dbacb4d8f6e9ec0a15d86a
Shubham <tiwarishubham635@gmail.com>
2025-10-13 16:21:49 +0530

    chore: remove auth from request.__str__() (#887)
    * chore: remove auth from request.__str__()
    
    * chore: remove auth from request.__str__()
    
    * chore: skip authorization key in headers

--- twilio/http/request.py
+++ twilio/http/request.py
@@ -56,10 +56,6 @@ class Request(object):
         )
 
     def __str__(self) -> str:
-        auth = ""
-        if self.auth and self.auth != Match.ANY:
-            auth = "{} ".format(self.auth)
-
         params = ""
         if self.params and self.params != Match.ANY:
             params = "?{}".format(urlencode(self.params, doseq=True))
@@ -75,11 +71,10 @@ class Request(object):
         headers = ""
         if self.headers and self.headers != Match.ANY:
             headers = "\n{}".format(
-                "\n".join(' -H "{}: {}"'.format(k, v) for k, v in self.headers.items())
+                "\n".join(' -H "{}: {}"'.format(k, v) for k, v in self.headers.items() if k.lower() != "authorization")
             )
 
-        return "{auth}{method} {url}{params}{data}{headers}".format(
-            auth=auth,
+        return "{method} {url}{params}{data}{headers}".format(
             method=self.method,
             url=self.url,
             params=params,
