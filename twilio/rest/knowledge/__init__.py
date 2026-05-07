41e04560b1087a9c71ab143108b30f47ea89001e
sburman <sburman@twilio.com>
2026-05-07 12:57:27 +0530

    fix init

--- twilio/rest/knowledge/__init__.py
+++ twilio/rest/knowledge/__init__.py
@@ -1,9 +1,4 @@
 from twilio.rest.knowledge.KnowledgeBase import KnowledgeBase
-from twilio.rest.knowledge.v1.knowledge import KnowledgeList as V1KnowledgeList
-from twilio.rest.knowledge.v2.chunk import ChunkList
-from twilio.rest.knowledge.v2.knowledge import KnowledgeList as V2KnowledgeList
-from twilio.rest.knowledge.v2.knowledge_basis import KnowledgeBasisList
-from twilio.rest.knowledge.v2.search import SearchList
 
 
 class Knowledge(KnowledgeBase):
